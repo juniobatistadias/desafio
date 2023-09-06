1d3f63e56d04f9b17b50a8b5495b6d0938cd1e44
juniobatistadias <kuhakujunioalvesbatistadias@gmail.com>
2023-09-06 16:11:50 -0300

    "exercicio de else online"

---
+++ exercicio2.py
@@ -0,0 +1,15 @@
+
+vogal=input("digite um caracter:")
+if vogal == "a":
+    print ("vogal")
+elif vogal == "e":
+    print ("vogal")
+elif vogal =="i":
+    print ("vogal")
+elif vogal == "o" :
+    print ("vogal")
+elif vogal == "u" :
+    print ("vogal")
+
+else :
+ print ("consoante")
---
+++ exerrcicio3.py
@@ -0,0 +1,17 @@
+nota1= float(input("Qual foi a primeira nota do aluno: "))
+nota2= float(input("Qual foi a segunda  nota do aluno: "))
+
+media=(nota1 + nota2)/2
+
+if media <= 7:
+  print("Reprovado")
+
+
+elif media < 10: 
+
+  print ("APROVADO")
+else :
+
+      print("Aprovado com Distinção")
+    
+ 
